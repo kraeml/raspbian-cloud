d9061df67a9972c83bb7adf79c10e91cd9b7fd3c
kraeml <kraeml@sesamestreet.franken.de>
2021-10-07 21:57:22 +0200

    Stage4 testing

--- stage4/00-tx-pi/files/tx-pi-setup-hostname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Not working in pi-gen
-#if [ "$HOSTNAME" == "raspberrypi" ]; then
-#    msg "Found default hostname, change it to 'tx-pi'"
-#    raspi-config nonint do_hostname tx-pi
-#    rm -f /etc/ssh/ssh_host_*
-#    ssh-keygen -A
-#fi
--- stage4/00-tx-pi/files/tx-pi-setup-usbmount-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# usbmount config
-cd /etc/usbmount
-wget -N https://raw.githubusercontent.com/ftCommunity/ftcommunity-TXT/3de48278d1260c48a0a20b07a35d14572c6248d3/board/fischertechnik/TXT/rootfs/etc/usbmount/usbmount.conf
